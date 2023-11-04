69924de0b78509ac176bcb07dd64a69e7a491c2c
Deinigu <diegolopezreduello@gmail.com>
2023-11-04 15:32:56 +0100

    ✨CREATE

--- elrastro_project/elrastroapp/urls.py
+++ elrastro_project/elrastroapp/urls.py
@@ -3,6 +3,7 @@ from elrastroapp import views
 
 urlpatterns = [
     path('api/usuarios', views.usuarios_list_view),
-    path('api/productos', views.productos_list),
-    path('api/productos/<str:idProducto>', views.productos_detail)
+    path('api/productos', views.productos_list_view),
+    path('api/productos/<str:idProducto>', views.productos_detail_view),
+    path('api/productos_create/', views.productos_create_view)
 ]
--- elrastro_project/elrastroapp/views.py
+++ elrastro_project/elrastroapp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 import pymongo
 from pymongo import ReturnDocument
 
+from datetime import datetime
+
 from bson import ObjectId
 from rest_framework.response import Response
 
@@ -36,7 +38,7 @@ def usuario_list():
 
 # Lista con todos los productos
 @api_view(['GET'])
-def productos_list(request):
+def productos_list_view(request):
     if request.method == 'GET':
         productos = collection_productos.find({})
         productos_serializer = ProductoSerializer(productos, many= True)
@@ -44,7 +46,48 @@ def productos_list(request):
     
 # Detalles de un producto
 @api_view(['GET'])
-def productos_detail(request, idProducto):
-    producto = collection_productos.find_one(ObjectId(idProducto))
-    producto_serializer = ProductoSerializer(producto, many = False) 
-    return JsonResponse(producto_serializer.data, safe=False)
+def productos_detail_view(request, idProducto):
+    if request.method == 'GET':
+        producto = collection_productos.find_one(ObjectId(idProducto))
+        producto_serializer = ProductoSerializer(producto, many = False) 
+        return JsonResponse(producto_serializer.data, safe=False)
+
+# Crear un producto
+@api_view(['POST'])
+def productos_create_view(request):
+    if request.method == 'POST':
+        now = datetime.now()
+        fecha = now.strftime("%d/%m/%Y")
+        
+        data = request.data
+                
+        nombre = data.get("Nombre")
+        descripcion = data.get("descripcion")
+        fotoURL = data.get("fotoURL")
+        precio = data.get("precio")
+        tags = data.get("tags")
+        ubicacion = data.get("ubicacion")
+        vendedor = data.get("vendedor")
+        cierre = data.get("cierre")
+        
+        producto = {
+            "_id" : ObjectId(),
+            "Nombre" : nombre,
+            "descripcion" : descripcion,
+            "fecha" : fecha,
+            "fotoURL" : fotoURL,
+            "precio" : precio,
+            "tags" : tags,
+            "ubicacion" : ubicacion,
+            "vendedor" : ObjectId(vendedor),
+            "cierre" : cierre,
+            "pujas" : []
+        }
+        
+        result = collection_productos.insert_one(producto)
+        if result.acknowledged:
+            # Document was successfully created, return its ObjectId
+            return Response({"id": str(result.inserted_id)}, status=status.HTTP_201_CREATED)
+        else:
+            # Failed to create the document
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
