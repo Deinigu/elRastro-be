4af0cd8d9b6204b6edadda78362fc61f5bf2b2ff
Deinigu <diegolopezreduello@gmail.com>
2023-11-04 22:12:21 +0100

    🔧Refactor /

--- elrastro_project/elrastroapp/urls.py
+++ elrastro_project/elrastroapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from elrastroapp import views
 
 urlpatterns = [
-    path('api/usuarios', views.usuarios_list_view),
-    path('api/productos', views.productos_list_view),
-    path('api/productos/<str:idProducto>', views.productos_detail_view),
+    path('api/usuarios/', views.usuarios_list_view),
+    path('api/productos/', views.productos_list_view),
+    path('api/productos/<str:idProducto>/', views.productos_detail_view),
     path('api/producto/create/', views.productos_create_view),
-    path('api/producto/delete/<str:idProducto>', views.delete_producto_view)
+    path('api/producto/delete/<str:idProducto>/', views.delete_producto_view)
 ]
