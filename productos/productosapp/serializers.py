56742e9e616d5e495ac11ddccdaa221421b0d05f
Deinigu <diegolopezreduello@gmail.com>
2023-11-07 13:16:32 +0100

    ✨Productos app

---
+++ productos/productosapp/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+class ProductoSerializer(serializers.Serializer):
+    _id = serializers.CharField(max_length = 24)
+    Nombre = serializers.CharField(max_length=100)
+    descripcion = serializers.CharField()
+    fecha = serializers.DateField(input_formats=["%d/%m/%Y"])
+    fotoURL = serializers.CharField()
+    precio = serializers.DecimalField(max_digits=6, decimal_places=2)
+    tags = serializers.CharField(max_length = 200)
+    ubicacion = serializers.CharField(max_length = 100)
+    vendedor = serializers.CharField(max_length = 24)
+    cierre = serializers.DateField(input_formats=["%d/%m/%Y"])
+    pujas = serializers.ListField(child=serializers.CharField())
