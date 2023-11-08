77a4ae6254b99802015799982feda635b07a75ac
Deinigu <diegolopezreduello@gmail.com>
2023-11-08 13:45:33 +0100

    🔧Quitado format del datetime de pujas

--- pujas/pujasapp/serializers.py
+++ pujas/pujasapp/serializers.py
@@ -4,5 +4,5 @@ class PujaSerializer(serializers.Serializer):
     _id = serializers.CharField()
     pujador = serializers.CharField()
     valor = serializers.DecimalField(max_digits=6, decimal_places=2)
-    fecha = serializers.DateTimeField(input_formats=["%d/%m/%Y"])
+    fecha = serializers.DateTimeField()
     producto = serializers.CharField()
