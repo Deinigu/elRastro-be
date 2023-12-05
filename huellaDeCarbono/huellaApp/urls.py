a3b69e833e228ee99894fe3250c1365ae589c672
FerRaw <fernandouni@ma.es>
2023-12-05 20:10:17 +0100

    Nueva Operacion Localizacion

--- huellaDeCarbono/huellaApp/urls.py
+++ huellaDeCarbono/huellaApp/urls.py
@@ -4,4 +4,5 @@ from huellaApp import views
 urlpatterns = [
     # USUARIOS
     path('api/huella/<str:idUsuario1>/<str:idUsuario2>/', views.huellaDeCarbono, name='huellaDeCarbono'),
+    path('api/coordenadas/<str:idUsuario>/', views.getCoordenadas),
 ]
