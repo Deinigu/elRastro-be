f7bcca1ea90571d4e65320eacfa1d0b27307927b
Alba Sánchez Ibáñez <91136148+brisanx@users.noreply.github.com>
2023-12-08 22:50:44 +0100

    🐛 Fixed views and urls for these mf classes

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from conversacionesapp import views
+
+urlpatterns = [
+    path('api/conversacion/create/', views.conversaciones_create),
+    path('api/conversacion/<str:conversacionId>', views.conversacion_detail),
+    path('api/conversacion/mensajes/<str:conversacionId>/', views.chats_list),
+    path('api/conversacion/id/<str:usuario1>/<str:usuario2>/', views.conversacion_get),
+    path('api/conversacion/add_chat/<str:conversacionId>/', views.chats_add),
+    path('api/conversacion/usuario/<str:usuarioId>/', views.conversaciones_list),
+]
+
+
