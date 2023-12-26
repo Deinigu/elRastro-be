b3c6a4e0c26217f51b3603d094f71c7718b240f5
Alba Sánchez Ibáñez <albasanchezibanez6@uma.es>
2023-12-26 21:07:37 +0100

    🦺 REFACTOR: Usuarios urls and views changed to follow REST

--- usuarios/usuariosapp/views.py
+++ usuarios/usuariosapp/views.py
@@ -29,27 +29,29 @@ dbname = my_client['ElRastro-SegundaEntrega']
 # Colecciones
 collection_usuarios = dbname["usuarios"]
 
-print(dbname.list_collection_names())
-
-# Create your views here.
 # -------------------------------------  VISTAS DE USUARIOS ----------------------------------------
 
-#Obtener la lista de usuarios y crear un usuario
+
+# ---------------------------------------  CRUD BÁSICO ----------------------------------------
+
+
+# Lista de usuarios y creación de un usuario
 @api_view(['GET', 'POST'])
 def lista_usuarios_crear(request):
     if request.method == 'GET':
         usuarios = list(collection_usuarios.find({}))
         for usuario in usuarios:
-            usuario['_id'] = str(usuario.get('_id',[]))
+            usuario['_id'] = str(usuario.get('_id', []))
             usuario['listaConver'] = [str(id) for id in usuario.get('listaConver', [])]
             usuario['productosVenta'] = [str(id) for id in usuario.get('productosVenta', [])]
-            
+
         serializer = UsuarioSerializer(data=usuarios, many=True)
         if serializer.is_valid():
             json_data = serializer.data
             return Response(json_data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'POST':
         serializer = UsuarioSerializer(data=request.data)
         if serializer.is_valid():
@@ -64,7 +66,7 @@ def lista_usuarios_crear(request):
             usuario['reputacion'] = 0.0
             result = collection_usuarios.insert_one(usuario)
             if result.acknowledged:
-                return Response({"message": "Usuario creado con éxito",},
+                return Response({"message": "Usuario creado con éxito"},
                                 status=status.HTTP_201_CREATED)
             else:
                 return Response({"error": "Algo salió mal. Usuario no creado."},
@@ -72,13 +74,13 @@ def lista_usuarios_crear(request):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#Obtener un usuario, borrar un usuario o actualizar un usuario
+# Obtener, borrar o actualizar un usuario
 @api_view(['GET', 'DELETE', 'PUT'])
 def view_usuario(request, usuario_id):
     usuario = collection_usuarios.find_one({'_id': ObjectId(usuario_id)})
     if usuario:
         if request.method == 'GET':
-            usuario['_id'] = str(ObjectId(usuario.get('_id',[])))
+            usuario['_id'] = str(ObjectId(usuario.get('_id', [])))
             usuario['listaConver'] = [str(ObjectId(id)) for id in usuario.get('listaConver', [])]
             usuario['productosVenta'] = [str(ObjectId(id)) for id in usuario.get('productosVenta', [])]
             serializer = UsuarioSerializer(data=usuario)
@@ -87,15 +89,16 @@ def view_usuario(request, usuario_id):
                 return Response(json_data, status=status.HTTP_200_OK)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
+
         elif request.method == 'DELETE':
             result = collection_usuarios.delete_one({'_id': ObjectId(usuario_id)})
             if result.acknowledged:
-                return Response({"message": "Usuario eliminado con éxito",},
+                return Response({"message": "Usuario eliminado con éxito"},
                                 status=status.HTTP_200_OK)
             else:
                 return Response({"error": "Algo salió mal. Usuario no eliminado."},
                                 status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
         elif request.method == 'PUT':
             serializer = UsuarioSerializer(data=request.data)
             if serializer.is_valid():
@@ -103,7 +106,7 @@ def view_usuario(request, usuario_id):
                 usuario['_id'] = ObjectId(usuario_id)
                 result = collection_usuarios.replace_one({'_id': ObjectId(usuario_id)}, usuario)
                 if result.acknowledged:
-                    return Response({"message": "Usuario actualizado con éxito",},
+                    return Response({"message": "Usuario actualizado con éxito"},
                                     status=status.HTTP_200_OK)
                 else:
                     return Response({"error": "Algo salió mal. Usuario no actualizado."},
@@ -112,154 +115,125 @@ def view_usuario(request, usuario_id):
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
         return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
     
 
-# -------------------------------------  BÚSQUEDAS PARAMETRIZADAS ----------------------------------------
+# ---------------------------------------  EXTRAS Y BÚSQUEDAS PARAMETRIZADAS ----------------------------------------
 
-#Añadir un producto a la lista de productos en venta de un usuario
-@api_view(['PUT'])
-def add_producto_venta_view(request, usuario_id, producto_id):
-    if request.method == 'PUT':
-        usuario = collection_usuarios.find_one({'_id': ObjectId(usuario_id)})
-        if usuario:
+# Añadir o eliminar un producto de la lista de productos en venta de un usuario
+@api_view(['PUT', 'DELETE'])
+def productos_venta_usuario_view(request, usuario_id, producto_id):
+    usuario = collection_usuarios.find_one({'_id': ObjectId(usuario_id)})
+
+    if usuario:
+        if request.method == 'PUT':
             usuario['productosVenta'].append(producto_id)
-            result = collection_usuarios.replace_one({'_id': ObjectId(usuario_id)}, usuario)
-            if result.acknowledged:
-                return Response({"message": "Producto añadido a la lista de productos en venta con éxito",},
-                                status=status.HTTP_200_OK)
-            else:
-                return Response({"error": "Algo salió mal. Producto no añadido a la lista de productos en venta."},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        else:
-            return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
-        
-#Eliminar un producto de la lista de productos en venta de un usuario
-@api_view(['PUT'])
-def delete_producto_venta_view(request, usuario_id, producto_id):
-    if request.method == 'PUT':
-        usuario = collection_usuarios.find_one({'_id': ObjectId(usuario_id)})
-        if usuario:
-            usuario['productosVenta'].remove(producto_id)
-            result = collection_usuarios.replace_one({'_id': ObjectId(usuario_id)}, usuario)
-            if result.acknowledged:
-                return Response({"message": "Producto eliminado de la lista de productos en venta con éxito",},
-                                status=status.HTTP_200_OK)
-            else:
-                return Response({"error": "Algo salió mal. Producto no eliminado de la lista de productos en venta."},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        else:
-            return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+        elif request.method == 'DELETE':
+            try:
+                usuario['productosVenta'].remove(producto_id)
+            except ValueError:
+                return Response({"error": "El producto no se encontraba en la lista de productos en venta."},
+                                status=status.HTTP_400_BAD_REQUEST)
 
-#Añade una conversacion a la lista de conversaciones de un usuario
-@api_view(['PUT'])
-def add_conversacion(request, usuario_id, conversacion_id):
-    if request.method == 'PUT':
-        print(usuario_id)
-        usuario = collection_usuarios.find_one({'_id': ObjectId(usuario_id)})
-        if usuario:
+        result = collection_usuarios.replace_one({'_id': ObjectId(usuario_id)}, usuario)
+        if result.acknowledged:
+            message = "Producto añadido a la lista de productos en venta con éxito" if request.method == 'PUT' else \
+                      "Producto eliminado de la lista de productos en venta con éxito"
+            return Response({"message": message}, status=status.HTTP_200_OK)
+        else:
+            error_message = "Algo salió mal. Producto no {} de la lista de productos en venta.".format(
+                "añadido" if request.method == 'PUT' else "eliminado")
+            return Response({"error": error_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+# Añadir o eliminar una conversación de la lista de conversaciones de un usuario
+@api_view(['PUT', 'DELETE'])
+def conversaciones_usuario_view(request, usuario_id, conversacion_id):
+    usuario = collection_usuarios.find_one({'_id': ObjectId(usuario_id)})
+
+    if usuario:
+        if request.method == 'PUT':
             usuario['listaConver'].append(conversacion_id)
-            result = collection_usuarios.replace_one({'_id': ObjectId(usuario_id)}, usuario)
-            if result.acknowledged:
-                return Response({"message": "Conversacion añadida a la lista de conversaciones con éxito",},
-                                status=status.HTTP_200_OK)
-            else:
-                return Response({"error": "Algo salió mal. Conversacion no añadida a la lista de conversaciones."},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        elif request.method == 'DELETE':
+            try:
+                usuario['listaConver'].remove(conversacion_id)
+            except ValueError:
+                return Response({"error": "La conversación no se encontraba en la lista de conversaciones."},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+        result = collection_usuarios.replace_one({'_id': ObjectId(usuario_id)}, usuario)
+        if result.acknowledged:
+            message = "Conversación añadida a la lista de conversaciones con éxito" if request.method == 'PUT' else \
+                      "Conversación eliminada de la lista de conversaciones con éxito"
+            return Response({"message": message}, status=status.HTTP_200_OK)
         else:
-            return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
-
-#Eliminar una conversacion de la lista de conversaciones de un usuario
-@api_view(['PUT'])
-def delete_conversacion(request, usuario_id, conversacion_id):
-    if request.method == 'PUT':
-        usuario = collection_usuarios.find_one({'_id': ObjectId(usuario_id)})
-        if usuario:
-            usuario['listaConver'].remove(conversacion_id)
-            result = collection_usuarios.replace_one({'_id': ObjectId(usuario_id)}, usuario)
-            if result.acknowledged:
-                return Response({"message": "Conversacion eliminada de la lista de conversaciones con éxito",},
-                                status=status.HTTP_200_OK)
-            else:
-                return Response({"error": "Algo salió mal. Conversacion no eliminada de la lista de conversaciones."},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        else:
-            return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
-
-
-# Buscar usuarios cuya reputación sea mayor de un número dado.
+            error_message = "Algo salió mal. Conversación no {} de la lista de conversaciones.".format(
+                "añadida" if request.method == 'PUT' else "eliminada")
+            return Response({"error": error_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+    
+# Usuarios con reputación mayor o menor que un número dado
 @api_view(['GET'])
-def usuarios_mayor_reputacion_view(request, reputacion):
-    if request.method == 'GET':
-        usuarios = list(collection_usuarios.find({'reputacion': {'$gt': float(reputacion)}}))
+def usuarios_reputacion_view(request, operador, reputacion):
+    operadores = {'mayor': '$gt', 'menor': '$lt'}
+
+    if request.method == 'GET' and operador in operadores:
+        condicion = {'reputacion': {operadores[operador]: float(reputacion)}}
+        usuarios = list(collection_usuarios.find(condicion))
+
         for usuario in usuarios:
-            usuario['_id'] = str(ObjectId(usuario.get('_id',[])))
+            usuario['_id'] = str(ObjectId(usuario.get('_id', [])))
             usuario['listaConver'] = [str(ObjectId(id)) for id in usuario.get('listaConver', [])]
             usuario['productosVenta'] = [str(ObjectId(id)) for id in usuario.get('productosVenta', [])]
-        usuario_serializer = UsuarioSerializer(data=usuarios , many=True)
-        if usuario_serializer.is_valid():
-            json_data = usuario_serializer.data 
+
+        serializer = UsuarioSerializer(data=usuarios, many=True)
+        if serializer.is_valid():
+            json_data = serializer.data
             return Response(json_data, status=status.HTTP_200_OK)
         else:
-            return Response(usuario_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({"error": "Operador no válido o método HTTP no permitido."},
+                        status=status.HTTP_400_BAD_REQUEST)
 
-# Buscar usuarios cuya reputación sea menor de un número dado.
+# Compradores de productos ofrecidos por un usuario
 @api_view(['GET'])
-def usuarios_menor_reputacion_view(request, reputacion):
+def compradores_usuario_view(request, usuario_id):
+    compradores = []
+
     if request.method == 'GET':
-        usuarios = list(collection_usuarios.find({'reputacion': {'$lt': float(reputacion)}}))
-        for usuario in usuarios:
-            usuario['_id'] = str(ObjectId(usuario.get('_id',[])))
-            usuario['listaConver'] = [str(ObjectId(id)) for id in usuario.get('listaConver', [])]
-            usuario['productosVenta'] = [str(ObjectId(id)) for id in usuario.get('productosVenta', [])]
-            serializer = UsuarioSerializer(data=usuario)
+        url_productos = f'http://localhost:8001/api/productos/anteriores/{usuario_id}'
+        response_productos = requests.get(url_productos)
+
+        if response_productos.status_code == 200:
+            productos = response_productos.json()
+
+            for producto in productos:
+                url_puja = f'http://localhost:8002/api/pujas/ultima_puja/producto/{str(producto["_id"])}'
+                response_puja = requests.get(url_puja)
+
+                if response_puja.status_code == 200:
+                    puja = response_puja.json()
+
+                    if puja['pujador'] not in compradores:
+                        compradores.append(ObjectId(puja['pujador']))
+
+            usuarios = list(collection_usuarios.find({'_id': {'$in': compradores}}))
+
+            for usuario in usuarios:
+                usuario['_id'] = str(ObjectId(usuario.get('_id', [])))
+                usuario['listaConver'] = [str(ObjectId(id)) for id in usuario.get('listaConver', [])]
+                usuario['productosVenta'] = [str(ObjectId(id)) for id in usuario.get('productosVenta', [])]
+
+            serializer = UsuarioSerializer(data=usuarios, many=True)
             if serializer.is_valid():
                 json_data = serializer.data
                 return Response(json_data, status=status.HTTP_200_OK)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        usuario_serializer = UsuarioSerializer(data=usuarios, many=True)
-        if usuario_serializer.is_valid():
-            json_data = usuario_serializer.data 
-            return Response(json_data, status=status.HTTP_200_OK)
-        else:
-            return Response(usuario_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-        
-# Devuelve quiénes han sido compradores de cualquiera de los productos ofrecidos por un determinado usuario
-@api_view(['GET'])
-def compradores_usuario_view(request, usuario_id):
-    compradores = []
-    if request.method == 'GET':
-        url = 'http://localhost:8001/api/productos/anteriores/' + usuario_id
-        response = requests.get(url)
-        if response.status_code == 200:
-            productos = response.json()
-            for producto in productos:
-                url = 'http://localhost:8002/api/pujas/ultima_puja/producto/' + str(producto['_id'])
-                response = requests.get(url)
-                if response.status_code == 200:
-                    puja = response.json()
-                    if puja['pujador'] not in compradores:
-                        compradores.append(ObjectId(puja['pujador']))
-            usuarios = list(collection_usuarios.find({'_id': {'$in': compradores}}))
-            for usuario in usuarios:
-                usuario['_id'] = str(ObjectId(usuario.get('_id',[])))
-                usuario['listaConver'] = [str(ObjectId(id)) for id in usuario.get('listaConver', [])]
-                usuario['productosVenta'] = [str(ObjectId(id)) for id in usuario.get('productosVenta', [])]
-                serializer = UsuarioSerializer(data=usuario)
-                if serializer.is_valid():
-                    json_data = serializer.data
-                    return Response(json_data, status=status.HTTP_200_OK)
-                else:
-                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            usuario_serializer = UsuarioSerializer(data=usuarios, many=True)
-            if usuario_serializer.is_valid():
-                json_data = usuario_serializer.data 
-                return Response(json_data, status=status.HTTP_200_OK)
-            else:
-                return Response(usuario_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({"error": "Productos no encontrados"}, status=status.HTTP_404_NOT_FOUND)
-    
+    else:
+        return Response({"error": "Método HTTP no permitido."}, status=status.HTTP_400_BAD_REQUEST)
