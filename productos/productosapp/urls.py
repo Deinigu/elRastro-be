661805e5ac52c73c02cd866d467bed0167488476
Alba Sánchez Ibáñez <albasanchezibanez6@uma.es>
2023-12-27 13:20:40 +0100

    🔧 REFACTOR: productos & base de datos link de servext

--- productos/productosapp/urls.py
+++ productos/productosapp/urls.py
@@ -2,15 +2,10 @@ from django.urls import path
 from productosapp import views
 
 urlpatterns = [
-    # PRODUCTOS
-    path('api/productos/', views.productos_list_view),
-    path('api/productos/create/', views.productos_list_view),
-    path('api/productos/update/<str:idProducto>/', views.producto_view),
-    path('api/productos/delete/<str:idProducto>/', views.producto_view),
-    path('api/productos/<str:idProducto>/', views.producto_view),
-    path('api/productos/anteriores/<str:idUsuario>/', views.productos_usuario_anterior_view),
-    path('api/productos/precio_menor/<str:precio>/', views.productos_menor_precio_view),
-    path('api/productos/add_puja/<str:idProducto>/<str:idPuja>/', views.add_puja_view),
-    path('api/productos/busqueda/<str:cadena>/', views.productos_busqueda_view),
+    path('api/productos/', views.productos_list_view, name='productos-list'),
+    path('api/productos/<str:idProducto>/', views.producto_detail_view, name='producto-detail'),
+    path('api/productos/anteriores/<str:idUsuario>/', views.productos_usuario_anterior_view, name='productos-usuario-anterior'),
+    path('api/productos/precio_menor/<str:precio>/', views.productos_menor_precio_view, name='productos-precio-menor'),
+    path('api/productos/add_puja/<str:idProducto>/<str:idPuja>/', views.add_puja_to_producto_view, name='add-puja-to-producto'),
+    path('api/productos/busqueda/<str:cadena>/', views.productos_busqueda_view, name='productos-busqueda'),
 ]
-    
